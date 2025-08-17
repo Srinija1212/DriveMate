af1f66e4109d0c771e5641a72dd254d90e5f2038
prabuteja12 <prabutejakunapareddy@gmail.com>
2025-08-17 12:40:42 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 // Entry point for the backend server
 
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
